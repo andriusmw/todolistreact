01437b67c9801d6a916945e332ac3306d59a7457
andriusmw <andriusmw@yahoo.es>
2022-03-14 15:12:50 +0100

    cambiando navbar y estilos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import logo from './logo.svg';
 import './App.css';
 
+
+  // eslint-disable-next-line jsx-a11y/anchor-is-valid
 function App() {
   return (
     <div className="App">
+      <nav className="navbar navbar-dark bg-dark">
       
+        <a href="" className="text-white">
+            Tasks
+        </a>
+
+      </nav>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Navegación</p>
-      
-     
     </div>
   );
 }
